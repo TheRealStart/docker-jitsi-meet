76cf768dce38524fc8689a60fdf45219755f4854
Ulugbek Nazarov <ulugbeknazarov15@gmail.com>
2020-06-29 14:57:11 +0500

    made changes to start_jigasi.sh script to stop failing with deps

--- start_jigasi.sh
+++ start_jigasi.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 envFile=".env"
 
-set -a && source $envFile && docker-compose -f jigasi.yml up -d
-
-# Restart other jitsi docker containers 
-# for environment variables to pass in
-docker-compose -f docker-compose-custom.yml up -d
+set -a && source $envFile && docker-compose -f docker-compose-custom.yml -f jigasi.yml up -d
