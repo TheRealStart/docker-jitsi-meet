33c783f0511d0a3f54f1fe29bcd577700ed3a980
Ulugbek Nazarov <ulugbeknazarov15@gmail.com>
2020-10-27 11:47:25 +0500

    fixed scripts and added more values in .gitignore

--- build-and-launch-jitsi.sh
+++ build-and-launch-jitsi.sh
@@ -14,27 +14,27 @@ if [[ -f $envFile ]]; then
     source ${envFile}
 
     # Config folders
-    echo "${BLUE}Creating config folders if not exist...${NC}"
+    echo -e "${BLUE}Creating config folders if not exist...${NC}"
     if ! sudo mkdir -p ${CONFIG}/{web/letsencrypt,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}; then
-        echo "{RED}Failed to create config folders!${NC}"
+        echo -e "${RED}Failed to create config folders!${NC}"
         sleep 5
         exit 1
     fi
 
     # Copy prosody custom prosody plugings 
-    echo "${BLUE}Copying mod_token_moderation.lua plugin into corresponding config folder...${NC}"
+    echo -e "${BLUE}Copying mod_token_moderation.lua plugin into corresponding config folder...${NC}"
     if ! sudo cp -r ./custom/prosody-plugins-custom/mod_token_moderation.lua ${CONFIG}/prosody/prosody-plugins-custom/; then
-        echo "{RED}Failed to copy the plugin!${NC}"
+        echo -e "${RED}Failed to copy the plugin!${NC}"
         sleep 5
         exit 1
     fi
 
-    echo "${BLUE}Building custom jitsi/web...${NC}"
+    echo -e "${BLUE}Building custom jitsi/web...${NC}"
     cd web
-    echo "${BLUE}Getting latest custom jitsi-meet...${NC}"
+    echo -e "${BLUE}Getting latest custom jitsi-meet...${NC}"
     if [ ! -d "trs-jitsi-meet" ]; then
-        if ! mkdir trs-jitsi-meet; then
-            echo "{RED}Failed to mkdir!${NC}"
+        if ! mkdir -p trs-jitsi-meet; then
+            echo -e "${RED}Failed to mkdir!${NC}"
             sleep 5
             exit 1
         fi
@@ -43,62 +43,63 @@ if [[ -f $envFile ]]; then
 
     # Pull/clone the latest changes from git repo
     if [ ! -d .git ]; then
+        rm -rf *
         if ! git clone github-trs-jitsi-meet:TheRealStart/jitsi-meet.git .; then
-            echo "{RED}Failed to git clone!${NC}"
+            echo -e "${RED}Failed to git clone!${NC}"
             sleep 5
             exit 1
         fi
         if ! git checkout "$CUSTOM_TRS_JITSI_MEET_BRANCH"; then
-            echo "{RED}Failed to git checkout!${NC}"
+            echo -e "${RED}Failed to git checkout!${NC}"
             sleep 5
             exit 1
         fi
     else
         if ! git fetch; then
-            echo "{RED}Failed to git fetch!${NC}"
+            echo -e "${RED}Failed to git fetch!${NC}"
             sleep 5
             exit 1
         fi
         if ! git checkout "$CUSTOM_TRS_JITSI_MEET_BRANCH"; then
-            echo "{RED}Failed to git checkout!${NC}"
+            echo -e "${RED}Failed to git checkout!${NC}"
             sleep 5
             exit 1
         fi
         if ! git pull; then
-            echo "{RED}Failed to git pull!${NC}"
+            echo -e "${RED}Failed to git pull!${NC}"
             sleep 5
             exit 1
         fi
     fi
 
     # Install npm packages
-    echo "${BLUE}Installing npm packages...${NC}"
+    echo -e "${BLUE}Installing npm packages...${NC}"
     if ! npm install; then
-        echo "{RED}Failed to npm install!${NC}"
+        echo -e "${RED}Failed to npm install!${NC}"
         sleep 5
         exit 1
     fi
 
     # Customize static files
-    echo "${BLUE}Customizing static files...${NC}"
+    echo -e "${BLUE}Customizing static files...${NC}"
     if ! rm -rf images/ title.html favicon.ico; then
-        echo "{RED}Failed to remove!${NC}"
+        echo -e "${RED}Failed to remove!${NC}"
         sleep 5
         exit 1
     fi
     cd ..
     if ! cp -r rootfs/defaults/custom/images/ trs-jitsi-meet/; then
-        echo "{RED}Failed to copy custom images!${NC}"
+        echo -e "${RED}Failed to copy custom images!${NC}"
         sleep 5
         exit 1
     fi
     if ! cp -r rootfs/defaults/custom/title.html trs-jitsi-meet/; then
-        echo "{RED}Failed to copy title.html!${NC}"
+        echo -e "${RED}Failed to copy title.html!${NC}"
         sleep 5
         exit 1
     fi
         if ! cp -r rootfs/defaults/custom/images/favicon.ico trs-jitsi-meet/; then
-        echo "{RED}Failed to copy favicon.ico!${NC}"
+        echo -e "${RED}Failed to copy favicon.ico!${NC}"
         sleep 5
         exit 1
     fi
@@ -106,27 +107,27 @@ if [[ -f $envFile ]]; then
     cd trs-jitis-meet
 
     # Compile with make and create source package
-    echo "${BLUE}Running make command...${NC}"
+    echo -e "${BLUE}Running make command...${NC}"
     if ! make; then
-        echo "{RED}Failed to make!${NC}"
+        echo -e "${RED}Failed to make!${NC}"
         sleep 5
         exit 1
     fi
     if ! make source-package; then
-        echo "{RED}Failed to make source-package!${NC}"
+        echo -e "${RED}Failed to make source-package!${NC}"
         sleep 5
         exit 1
     fi
 
     # Unpack and build jitsi-meet
-    echo "${BLUE}Unpacking and building jitsi-meet...${NC}"
+    echo -e "${BLUE}Unpacking and building jitsi-meet...${NC}"
     if ! tar -xzvf trs-jitsi-meet/jitsi-meet.tar.bz2; then
-        echo "{RED}Failed to untar!${NC}"
+        echo -e "${RED}Failed to untar!${NC}"
         sleep 5
         exit 1
     fi
     if ! docker build --tag jitsi/web:custom .; then
-        echo "{RED}Failed to build docker image!${NC}"
+        echo -e "${RED}Failed to build docker image!${NC}"
         sleep 5
         exit 1
     fi
@@ -135,7 +136,7 @@ if [[ -f $envFile ]]; then
     cd ..
     docker-compose -f docker-compose-custom.yml up -d --force-recreate
 else
-    echo "${RED}.env file not found${NC}"
+    echo -e "${RED}.env file not found${NC}"
     sleep 5
     exit 1
 fi
