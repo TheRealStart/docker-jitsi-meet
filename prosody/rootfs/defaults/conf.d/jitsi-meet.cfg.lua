199ccee9c352f62828fa81484c0b9769a58a2fc4
Ulugbek Nazarov <ulugbeknazarov15@gmail.com>
2020-11-27 19:47:59 +0500

    added websocket module to prosody if enabled

--- prosody/rootfs/defaults/conf.d/jitsi-meet.cfg.lua
+++ prosody/rootfs/defaults/conf.d/jitsi-meet.cfg.lua
@@ -109,6 +109,9 @@ VirtualHost "{{ .Env.XMPP_DOMAIN }}"
         {{ if .Env.ENABLE_TURNCREDENTIALS }}
         "turncredentials";
         {{ end }}
+        {{ if .Env.ENABLE_WEBSOCKETS }}
+        "websocket";
+        {{ end }}
     }
 
     {{ if and $ENABLE_LOBBY (not $ENABLE_GUEST_DOMAIN) }}
